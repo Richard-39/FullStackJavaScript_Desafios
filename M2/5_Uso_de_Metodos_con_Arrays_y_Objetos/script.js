c3c74379edb5b30e6645e1114cd892a1b5225393
Ricardo Abarza <ricardo.abarza.v@zoho.com>
2021-05-25 22:36:27 -0400

    m2 desafio arrays

---
+++ M2/5_Uso_de_Metodos_con_Arrays_y_Objetos/script.js
@@ -0,0 +1,199 @@
+var radiologia = [
+    {
+        hora:"11:00",
+        especialista:"IGNACIO SCHULZ",
+        paciente:"FRANCISCA ROJAS",
+        rut:"9878782-1",
+        prevision:"FONASA"
+    },
+    {
+        hora:"11:30",
+        especialista:"FEDERICO SUBERCASEAUX",
+        paciente:"PAMELA ESTRADA",
+        rut:"15345241-3",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"15:00",
+        especialista:"FERNANDO WURTHZ",
+        paciente:"ARMANDO LUNA",
+        rut:"16445345-9",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"15:30",
+        especialista:"ANA MARIA GODOY",
+        paciente:"MANUEL GODOY",
+        rut:"17666419-0",
+        prevision:"FONASA"
+    },
+    {
+        hora:"16:00",
+        especialista:"PATRICIA SUAZO",
+        paciente:"RAMON ULLOA",
+        rut:"14989389-K",
+        prevision:"FONASA"
+    }
+];
+var traumatologia = [
+    {
+        hora:"8:00",
+        especialista:"MARIA PAZ ALTUZARRA",
+        paciente:"PAULA SANCHEZ",
+        rut:"15554774-5",
+        prevision:"FONASA"
+    },
+    {
+        hora:"10:00",
+        especialista:"RAUL ARAYA",
+        paciente:"ANGÉLICA NAVAS",
+        rut:"15444147-9",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"10:30",
+        especialista:"MARIA ARRIAGADA",
+        paciente:"ANA KLAPP",
+        rut:"17879423-9",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"11:00",
+        especialista:"ALEJANDRO BADILLA",
+        paciente:"FELIPE MARDONES",
+        rut:"1547423-6",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"11:30",
+        especialista:"CECILIA BUDNIK",
+        paciente:"DIEGO MARRE",
+        rut:"16554741-K",
+        prevision:"FONASA"
+    },
+    {
+        hora:"12:00",
+        especialista:"ARTURO CAVAGNARO",
+        paciente:"CECILIA MENDEZ",
+        rut:"9747535-8",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"12:30",
+        especialista:"ANDRES KANACRI",
+        paciente:"MARCIAL SUAZO",
+        rut:"11254785-5",
+        prevision:"ISAPRE"
+    }
+];
+var dental = [
+    {
+        hora:"8:30",
+        especialista:"ANDREA ZUÑIGA",
+        paciente:"MARCELA RETAMAL",
+        rut:"11123425-6",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"11:00",
+        especialista:"MARIA PIA ZAÑARTU",
+        paciente:"ANGEL MUÑOZ",
+        rut:"9878789-2",
+        prevision:"ISAPRE"
+    },
+    {
+        hora:"11:30",
+        especialista:"SCARLETT WITTING",
+        paciente:"MARIO KAST",
+        rut:"7998789-5",
+        prevision:"FONASA"
+    },
+    {
+        hora:"13:00",
+        especialista:"FRANCISCO VON TEUBER",
+        paciente:"KARIN FERNANDEZ",
+        rut:"18887662-K",
+        prevision:"FONASA"
+    },
+    {
+        hora:"13:30",
+        especialista:"EDUARDO VIÑUELA",
+        paciente:"HUGO SANCHEZ",
+        rut:"17665461-4",
+        prevision:"FONASA"
+    },
+    {
+        hora:"14:00",
+        especialista:"RAQUEL VILLASECA",
+        paciente:"ANA SEPULVEDA",
+        rut:"14441281-0",
+        prevision:"ISAPRE"
+    }
+];
+
+document.write(`<p>${radiologia[0].paciente} - ${radiologia[0].prevision} | Última atención: ${radiologia[radiologia.length-1].paciente} - ${radiologia[radiologia.length-1].prevision} </p>`);
+document.write(`<p>${traumatologia[0].paciente} - ${traumatologia[0].prevision} | Última atención: ${traumatologia[traumatologia.length-1].paciente} - ${traumatologia[traumatologia.length-1].prevision} </p>`);
+document.write(`<p>${dental[0].paciente} - ${dental[0].prevision} | Última atención: ${dental[dental.length-1].paciente} - ${dental[dental.length-1].prevision}</p></br>`);
+
+document.write("<h3>Radiología</h3>");
+
+document.write('<table style="width:100%">');
+    document.write('<tr>');
+        document.write('<th>Hora</th>');
+        document.write('<th>Especialista</th>');
+        document.write('<th>Paciente</th>');
+        document.write('<th>Rut</th>');
+        document.write('<th>Previsión</th>');
+    document.write('</tr>');
+    for (atencion of radiologia){
+        document.write('<tr>');
+            document.write(`<td>${atencion.hora}</td>`);
+            document.write(`<td>${atencion.especialista}</td>`);
+            document.write(`<td>${atencion.paciente}</td>`);
+            document.write(`<td>${atencion.rut}</td>`);
+            document.write(`<td>${atencion.prevision}</td>`);
+        document.write('</tr>');
+    }
+document.write('</table></br>');
+
+document.write("<h3>Traumatología</h3>");
+
+document.write('<table style="width:100%">');
+    document.write('<tr>');
+        document.write('<th>Hora</th>');
+        document.write('<th>Especialista</th>');
+        document.write('<th>Paciente</th>');
+        document.write('<th>Rut</th>');
+        document.write('<th>Previsión</th>');
+    document.write('</tr>');
+    for (atencion of traumatologia){
+        document.write('<tr>');
+            document.write(`<td>${atencion.hora}</td>`);
+            document.write(`<td>${atencion.especialista}</td>`);
+            document.write(`<td>${atencion.paciente}</td>`);
+            document.write(`<td>${atencion.rut}</td>`);
+            document.write(`<td>${atencion.prevision}</td>`);
+        document.write('</tr>');
+    }
+document.write('</table></br>');
+
+document.write("<h3>Dental</h3></br>");
+
+document.write('<table style="width:100%">');
+    document.write('<tr>');
+        document.write('<th>Hora</th>');
+        document.write('<th>Especialista</th>');
+        document.write('<th>Paciente</th>');
+        document.write('<th>Rut</th>');
+        document.write('<th>Previsión</th>');
+    document.write('</tr>');
+    for (atencion of dental){
+        document.write('<tr>');
+            document.write(`<td>${atencion.hora}</td>`);
+            document.write(`<td>${atencion.especialista}</td>`);
+            document.write(`<td>${atencion.paciente}</td>`);
+            document.write(`<td>${atencion.rut}</td>`);
+            document.write(`<td>${atencion.prevision}</td>`);
+        document.write('</tr>');
+    }
+document.write('</table></br>');
